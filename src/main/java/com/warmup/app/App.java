7c6199344aeda391e30618c8574c1d23ed33442e
K.M. Tang <tangkm@users.noreply.github.com>
2014-07-07 14:26:38 +0100

    first commit

---
+++ src/main/java/com/warmup/app/App.java
@@ -0,0 +1,13 @@
+package com.warmup.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Rusty!" );
+    }
+}
